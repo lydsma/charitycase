a029c759d2814ec0200797aa473de65e6fc08e34
Lyd <lydiama8@gmail.com>
2020-04-02 00:12:47 -0700

    Revert "set up mf mgngo" forgot to remove smth lol
    This reverts commit 6cb9dfa17c838ddc09a433851e814fdad8b09b4c.

--- app.js
+++ app.js
@@ -1,19 +1,14 @@
 var createError = require('http-errors')
+var express = require('express');
 var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
-
-// set up Express
-var express = require('express');
 var app = express();
 
-// set up EJS
+// view engine setup
+app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
 
-// set up BodyParser
-var bodyParser = require('body-parser');
-app.use(bodyParser.urlencoded({ extended: true }));
-app.set('views', path.join(__dirname, 'views')); // view engine setup
 app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
@@ -21,103 +16,18 @@ app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 app.use(express.static(path.join(__dirname, 'node_modules')));
 
-// import route handlers
-var userRouter = require('./routes/userRoutes');
-
-// import the Person class from Person.js
-var Person = require('./database/database.js');
+//var indexRouter = require('./routes/index');
+//var usersRouter = require('./routes/users');
 //var db = require('./database/database.js');
 
-/*************************************************/
+//begin routes 
 
-// route for creating a new person via "create new person" form
-app.use('/create', userRouter);
+//app.use('/', indexRouter);
+//app.use('/users', usersRouter);
 
-// route for showing all the people
-app.use('/all', (req, res) => {
-    
-	// find all the Person objects in the database
-	Person.find( {}, (err, persons) => {
-		if (err) {
-		    res.type('html').status(200);
-		    console.log('uh oh' + err);
-		    res.write(err);
-		}
-		else {
-		    if (persons.length == 0) {
-			res.type('html').status(200);
-			res.write('There are no people');
-			res.end();
-			return;
-		    }
-		    // use EJS to show all the people
-		    res.render('all', { persons: persons });
-
-		}
-	    }).sort({ 'age': 'asc' }); // this sorts them BEFORE rendering the results
-    });
-
-// route for accessing data via the web api
-// to use this, make a request for /api to get an array of all Person objects
-// or /api?name=[whatever] to get a single object
-app.use('/api', (req, res) => {
-	console.log("LOOKING FOR SOMETHING?");
-
-	// construct the query object
-	var queryObject = {};
-	if (req.query.name) {
-	    // if there's a name in the query parameter, use it here
-	    queryObject = { "name" : req.query.name };
-	}
-    
-	Person.find( queryObject, (err, persons) => {
-		console.log(persons);
-		if (err) {
-		    console.log('uh oh' + err);
-		    res.json({});
-		}
-		else if (persons.length == 0) {
-		    // no objects found, so send back empty json
-		    res.json({});
-		}
-		else if (persons.length == 1 ) {
-		    var person = persons[0];
-		    // send back a single JSON object
-		    res.json( { "name" : person.name , "age" : person.age } );
-		}
-		else {
-		    // construct an array out of the result
-		    var returnArray = [];
-		    persons.forEach( (person) => {
-			    returnArray.push( { "name" : person.name, "age" : person.age } );
-			});
-		    // send it back as JSON Array
-		    res.json(returnArray); 
-		}
-		
-	    });
-    });
-
-
-/*************************************************/
-
-app.use('/public', express.static('public'));
-
-app.all('/', (req, res) => { res.redirect('/public/personform.html'); } );
-
-module.exports = app;
-
-/** 
- *
-
- * 
- * // server listener
-app.listen(3000,  () => {
-	console.log('Listening on port 3000');
-	});
-	
-  app.use('/', function(req, res, next) {
+app.use('/', function(req, res, next) {
   res.send('<strong>!!!!! success !!!!! </strong>');
+});
 
 // catch 404 and forward to error handler
 app.use(function (req, res, next) {
@@ -135,4 +45,4 @@ app.use(function (err, req, res, next) {
   res.render('error');
 });
 
-}); */
+module.exports = app;
--- database/database.js
+++ database/database.js
@@ -1,25 +1,91 @@
-var mongoose = require('mongoose');
+// SET UP DEV ENV
 
-// Connect to MongoDB
-mongoose.connect('mongodb+srv://dbUser350:mn%25Q3e5%24@cluster0-q1ukr.gcp.mongodb.net/test?retryWrites=true&w=majority', { useNewUrlParser: true, useUnifiedTopology: true });
-var db = mongoose.connection;
-db.on('error', console.error.bind(console, 'connection error:'));
-db.once('open', function() {
-  // we're connected!
-  console.info('connected to db');
-});
+// Add Firebase + Cloud Firestore libs added to app (npm install)
+// Manually require Firebase and Cloud Firestore
+const firebase = require("firebase");
+// Required for side-effects
+require("firebase/firestore");
 
-var Schema = mongoose.Schema;
+// INITIALIZE FS through FB
+var firebaseConfig = {
+    apiKey: "AIzaSyB8V3IYwqKQRk433SLSIDwlthJt7T2qmbI",
+    authDomain: "dataanalysis-e8abe.firebaseapp.com",
+    databaseURL: "https://dataanalysis-e8abe.firebaseio.com",
+    projectId: "dataanalysis-e8abe",
+    storageBucket: "dataanalysis-e8abe.appspot.com",
+    messagingSenderId: "442052096901",
+    appId: "1:442052096901:web:ff98e8966965bcc1370aec",
+    measurementId: "G-C5F0M5CD4S"
+};
+// Initialize Firebase
+firebase.initializeApp(firebaseConfig);
+var db = firebase.firestore();
 
-var personSchema = new Schema({
-	name: {type: String, required: true, unique: true},
-	age: Number
-    });
+//-------------------------------------------------//
+var myDB_getTravelTime = function (startCity, startInt, endInt, timeOfDay, callback) {
+    console.log('Getting average time of' + startInt + ' to ' + endInt + ' at ' + timeOfDay);
+    var source = db.collection(startCity);  //Detroit or Windsor
+    var results = null;
+    
+    source.where("Source_Name", "==", startInt).where("Destination_name", "==", endInt)
+        .get()
+        .then(function (querySnapshot) {
+            querySnapshot.forEach(function (doc) {
+                var document = doc.data();
+                var entryTimeOfDay = doc.get("Time_of_day");   //entry time of day
 
-// export personSchema as a class called Person
-module.exports = mongoose.model('Person', personSchema);
+                if (timeOfDay == entryTimeOfDay) {     //if the time the person is travelling is within this binned interval....
+                    console.log('success');
+                    console.log(doc.id, " => ", document);
+                    avgSecs = doc.get("Travel_time");   //return average in seconds
 
-personSchema.methods.standardizeName = function() {
-    this.name = this.name.toLowerCase();
-    return this.name;
+                    var measuredTime = new Date(null);
+                    measuredTime.setSeconds(Math.round(avgSecs)); //convert from double to int
+                    var results = measuredTime.toISOString().substr(11, 8);
+                    console.log(results);
+                    callback(results, null);
+                }
+            });
+        })
+        .catch(function (error) {
+            console.log("Error getting documents: ", error);
+            callback(null, error);
+        });
+
+    //callback(results, null);
 }
+
+var findTimeOfDay = function(time, callback) {
+    var selection = new Date(time);
+    if (selection == null) {
+        callback(null, 'thing was null');
+    } else {
+        var hour = selection.getHours();
+        var timeOfDay;
+        if (hour >= 0 && hour < 6) { // 12am to 6am late
+            timeOfDay = 'late';
+        } else if (hour >= 6 && hour < 9) { // 6am to 9am morning
+            timeOfDay = 'morning';
+        } else if (hour >= 9 && hour < 12) { // 9am to 12pm midMorning
+            timeOfDay = 'midMorning';
+        } else if (hour >= 12 && hour < 17) { // 12pm to 5pm afternoon
+            timeOfDay = 'afternoon';
+        } else if (hour >= 17 && hour < 21) { //5pm to 9pm dinner
+            timeOfDay = 'dinner';
+        } else {                            //night
+            timeOfDay = 'night';
+        }
+        console.log('tod: ' + timeOfDay);
+        callback(timeOfDay,null);
+    } 
+}
+
+//myDB_getTravelTime("Detroit", "Dragoon and Fisher Fwy Ser Drs", "Totten Street and Huron Church Road", "2018-06-12T19:30");
+//findTimeOfDay("Detroit", "Dragoon and Fisher Fwy Ser Drs", "Totten Street and Huron Church Road", "2018-06-12T19:30", myDB_getTravelTime);
+
+let database = {
+    getTimeOfDay: findTimeOfDay,
+    getTravelTime: myDB_getTravelTime
+}
+
+module.exports = database;
