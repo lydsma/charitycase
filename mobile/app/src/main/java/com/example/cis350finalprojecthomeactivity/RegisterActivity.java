1b5d6e63552c7189a8cb0acc7f5225b1ca04077f
stevshap <stevshap@seas.upenn.edu>
2020-04-19 21:21:32 -0400

    need mongo help

--- mobile/app/src/main/java/com/example/cis350finalprojecthomeactivity/RegisterActivity.java
+++ mobile/app/src/main/java/com/example/cis350finalprojecthomeactivity/RegisterActivity.java
@@ -1,8 +1,11 @@
 package com.example.cis350finalprojecthomeactivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
+import android.widget.EditText;
 import android.widget.Spinner;
 
 import androidx.appcompat.app.AppCompatActivity;
@@ -11,6 +14,12 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class RegisterActivity extends AppCompatActivity {
+
+    public static final String EMAIL = "EMAIL";
+    private EditText first;
+    private EditText last;
+    private EditText em;
+    private EditText pass;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -45,5 +54,31 @@ public class RegisterActivity extends AppCompatActivity {
 
         donSpinner.setOnItemSelectedListener((AdapterView.OnItemSelectedListener) this);
         donSpinner.setAdapter(donAdapter);
+    }
+
+    public void onSubmitButtonClick(View v) {
+        Intent i = new Intent(this, MainActivity.class);
+
+        first = (EditText) findViewById(R.id.storeFirstName);
+        String firstName = first.getText().toString();
+
+        last = (EditText) findViewById(R.id.storeLastName);
+        String LastName = last.getText().toString();
+
+        em = (EditText) findViewById(R.id.storeEmail);
+        String userEmail = em.getText().toString();
+
+        pass = (EditText) findViewById(R.id.storePassword);
+        String password = pass.getText().toString();
+
+        Spinner catSpinner = (Spinner) findViewById(R.id.categorySpinner);
+        String category = catSpinner.getSelectedItem().toString();
+
+        Spinner donSpinner = (Spinner) findViewById(R.id.userSpinner);
+        String donorType = donSpinner.getSelectedItem().toString();
+
+        i.putExtra(EMAIL, userEmail);
+
+        startActivity(i);
     }
 }
