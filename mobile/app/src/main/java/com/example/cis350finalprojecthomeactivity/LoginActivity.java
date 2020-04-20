c3248c518724d5ef653b0bbdc2af80b4a394adc6
stevshap <stevshap@seas.upenn.edu>
2020-04-19 20:45:18 -0400

    backend for registration (not mongo integrated)

--- mobile/app/src/main/java/com/example/cis350finalprojecthomeactivity/LoginActivity.java
+++ mobile/app/src/main/java/com/example/cis350finalprojecthomeactivity/LoginActivity.java
@@ -1,4 +1,19 @@
 package com.example.cis350finalprojecthomeactivity;
 
-public class LoginActivity {
+import android.os.Bundle;
+import android.widget.Spinner;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class LoginActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.login_view);
+
+    }
 }
