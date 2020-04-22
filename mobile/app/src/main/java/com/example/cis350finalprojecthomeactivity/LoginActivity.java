f9959fcdf1bc4e19858e3ddc7e2c46a94d519a4a
stevshap <stevshap@seas.upenn.edu>
2020-04-22 19:49:22 -0400

    this does not work

--- mobile/app/src/main/java/com/example/cis350finalprojecthomeactivity/LoginActivity.java
+++ mobile/app/src/main/java/com/example/cis350finalprojecthomeactivity/LoginActivity.java
@@ -1,17 +1,25 @@
 package com.example.cis350finalprojecthomeactivity;
 
 import android.content.Intent;
+import android.os.AsyncTask;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.EditText;
 import android.widget.Toast;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import java.io.IOException;
+import java.net.HttpURLConnection;
+import java.net.MalformedURLException;
+import java.net.URL;
 import java.util.ArrayList;
 import java.util.HashSet;
 import java.util.Hashtable;
 import java.util.List;
+import java.util.Scanner;
+import java.util.concurrent.ExecutionException;
 
 public class LoginActivity extends AppCompatActivity {
 
@@ -46,6 +54,32 @@ public class LoginActivity extends AppCompatActivity {
 
         pass = findViewById(R.id.password);
         String password = pass.getText().toString();
+
+        try {
+            URL url = new URL("http://10.0.2.2:3000/mobilechecklogin");
+            AsyncTask<URL, String, String> task = new LoginActivity.LoginUser();
+            task.execute(url);
+
+            // get the response and Toast it
+            String msg = task.get();
+
+            //@stev and ozzi this is 4 if u need to covert to json array
+            /**
+             JSONObject arrayOfPosts = new JSONObject(msg);
+             JSONArray posts = arrayOfPosts.getJSONArray("posts");
+             */
+
+            Toast.makeText(this, msg, Toast.LENGTH_LONG).show();
+
+        } catch (MalformedURLException e) {
+            e.printStackTrace();
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        } catch (ExecutionException e) {
+            e.printStackTrace();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
 
         allUsersArray = new ArrayList<LoginActivity.User>();
         allEmails = new HashSet<String>();
@@ -97,5 +131,43 @@ public class LoginActivity extends AppCompatActivity {
 
     private enum Donor {
         DONOR, RECIPIENT, EMPTY
+    }
+
+    class LoginUser extends AsyncTask<URL, String, String> {
+
+        @Override
+        protected String doInBackground(URL... urls) {
+
+
+            try {
+                URL url = urls[0];
+
+                HttpURLConnection connect = (HttpURLConnection) url.openConnection();
+
+                // send http GET request to server
+                connect.setRequestMethod("GET");
+                connect.connect();
+
+                // read response using Scanner
+                Scanner in = new Scanner(url.openStream());
+                String msg = "";
+
+                while (in.hasNext()) {
+                    msg = in.useDelimiter("\\A").next();
+                }
+
+                // JSONObject arrayOfPosts = new JSONObject(msg);
+
+                // turn arrayOfPosts into allPosts
+                Log.v("string results", msg);
+                return msg;
+
+            } catch (IOException e) {
+                return e.toString();
+            } catch (Exception e) {
+                return e.toString();
+            }
+        }
+
     }
 }
